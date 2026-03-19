77f7d597328060debb8d5cdb6511c8fb9fd5aeca
soundaryavsb <soundaryadamotharan@gmail.com>
2026-03-19 22:21:16 +0530

    Login functionality updated

--- LetcodeCucumber/src/test/java/gitBranchCheck.java
+++ LetcodeCucumber/src/test/java/gitBranchCheck.java
@@ -3,6 +3,7 @@ public class gitBranchCheck {
 
 	public static void main(String[] args) {
 		   System.out.println("Login functionality automated");
+		   System.out.println("Branch check");
 
 	}
 
